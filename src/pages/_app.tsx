49bce9a3e09e27cb145710cea66cf04d523145b2
Mohammad Ghorab <mody.moustafa@yahoo.com>
2023-04-19 21:49:22 +0200

    Add favicon to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import "@styles/globals.scss";
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+
+import '@styles/globals.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Cre[ai]tion</title>
+        <meta name="description" content="Cre[ai]tion" />
+        <link rel="icon" href="/cre_ai_tion_logo_black.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
